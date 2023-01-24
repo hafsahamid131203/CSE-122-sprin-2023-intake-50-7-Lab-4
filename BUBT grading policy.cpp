7858d1dd65e1e5e4c84b0608c3d524a52fd71f1a
Hafsa Binte Hamid <122333759+hafsahamid131203@users.noreply.github.com>
2023-01-24 07:12:20 +0600

    Add files via upload

---
+++ BUBT grading policy.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Please enter the numbers of BUBT students: ";
+    cin>>num;
+    if
+        num>=80
+        cout<<"Enter A+\n: ";
+        cin>>A+;
+    else if
+        num>=75 && num<79
+        cout<<"Enter A\n: ";
+        cin>>A;
+    else if
+        num>=70 && num<74
+        cout<<"Enter A-\n: ";
+        cin>>A-;
+    else if
+        num>=65 && num<69
+        cout<<"Enter B+\n: ";
+        cin>>B+;
+    else if
+        num>=60 && num<64
+        cout<<"Enter B\n: ";
+        cin>>B;
+    else if
+        num>=55 && num<59
+        cout<<"Enter B-\n: ";
+        cin>>B-;
+    else if
+        num>=50 && num<54
+        cout<<"Enter C+\n: ";
+        cin>>C+;
+    else if
+        num>=45 && num<49
+        cout<<"Enter C\n: ";
+        cin>>C;
+    else if
+        num>=40 && num<44
+        cout<<"Enter D\n: ";
+        cin>>D;
+     else
+        num<40
+        cout<<"Enter F\n: ";
+        cin>>F;
+    return 0;
+}
